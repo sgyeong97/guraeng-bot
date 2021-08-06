b01d0c660e546ebeb8b7df3ee0220ebba6b31e9e
guraeng <sgyeong97@gmail.com>
2021-08-06 17:39:22 +0900

    add prefixes change

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ from discord.ext import commands, tasks
 from itertools import cycle
 #키워드 알림기에 필요한 것 import
 
-prefix = "#"
-bot = commands.Bot(command_prefix = prefix)
 dream = cycle(["꿈나라 여행-", "잠은 잘 자야해", "잘자고 일어나서 봐"])
 work = cycle(["구랭 굴러다니며 활동","데굴데굴","공과 물아일체"])
+
+def get_prefix(bot, message):
+    with open(f'{./prefixes.json', "r") as f:
+        prefixes = json.load(f)
+    if str(message.guild.id) not in prefixes:
+        return commands.when_mentioned_or('#')(bot, message)
+
+    prefix = prefixes[str(message.guild.id)]
+    return commands.when_mentioned_or(prefix)(bot, message)
+    
+bot = commands.Bot(command_prefix = get_prefix)
+
 
 @tasks.loop(seconds=30)
 async def status_start():
@@ -37,6 +47,39 @@ async def getup(ctx):
 async def hello(ctx):
     print("say hello")
     await ctx.send("hello")
+
+#명령어 접두사 변경
+@bot.command(name="접두사")
+async def change_prefix(ctx, new_prefix=None):
+    if not ctx.guild:
+        await ctx.send("DM기능 미지원")
+        return
+    if new_prefix is None:
+        await ctx.send("공백은 지원하지 않습니다.")
+        return
+    with open(f'./prefixes.json','r') as f:
+        prefixes = json.load(f)
+        prefixes[str(ctx.guild.id)] = prefix
+    with open(f'{./prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=' ')
+    await ctx.send(f"{new_prefix}로 변경되었습니다.")
+#접두사 초기화
+@bot.command(name="접두사_초기화")
+async def change_prefix(ctx):
+    if not ctx.guild:
+        await ctx.send("DM기능 미지원")
+        return
+    with open(f'./prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    if str(ctx.guild.id) not in prefixes:
+        await ctx.send("초기의 # 접두사 그대로 입니다.")
+        return
+    prefixes.pop(ctx.guild.id)
+
+    with open(f'./prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=' ')
+
+    await ctx.send("접두사 '#'로 초기화 완료")
 
 @bot.command()
 async def join(ctx):
